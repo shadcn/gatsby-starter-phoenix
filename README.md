62bae4d47ab7d18ddfd6fcd17f1d056acbd78070
Arshad Chummun <arshad@chummun.com>
2019-12-01 01:27:05 +0400

    chore(release): publish
     - @arshad/gatsby-theme-blog-core@1.0.1
     - @arshad/gatsby-theme-core@1.0.1
     - @arshad/gatsby-theme-page-core@1.0.1
     - @arshad/gatsby-theme-photo-core@1.0.1
     - @arshad/gatsby-theme-podcast-core@1.0.1
     - @arshad/gatsby-theme-portfolio-core@1.0.1
     - @arshad/gatsby-example-phoenix@1.0.1
     - @arshad/gatsby-source-podcast@1.0.1
     - @arshad/gatsby-starter-phoenix@1.0.1
     - @arshad/gatsby-theme-phoenix@1.0.1

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# Gatsby Starter Phoenix
-A Gatsby starter for Phoenix - See https://github.com/arshad/gatsby-theme-phoenix
+<div align="center">
+<h1>@arshad/gatsby-starter-phoenix</h1>
+</div>
+
+<p align="center">
+  <a href="https://arshad-gatsby-example-phoenix.netlify.com"><img src="https://img.shields.io/badge/demo-netlify-success" alt="Demo"></a>
+<a href="https://github.com/arshad/gatsby-themes/tree/master/themes/gatsby-theme-phoenix"><img src="https://img.shields.io/badge/%40arshad%2Fgatsby--themes-phoenix-blueviolet" alt="Theme"></a>
+</p>
+
+<p align="center">
+A Gatsby starter to kick off a site using the <a href="https://github.com/arshad/gatsby-themes/tree/master/themes/gatsby-theme-phoenix">@arshad/gatsby-theme-phoenix</a> theme.
+</p>
+
+<p align="center">
+  <img src="https://i.imgur.com/BufJgj8.png" alt="Phoenix preview" />
+</p>
+
+<h3 align="center"><code>gatsby new site arshad/gatsby-starter-phoenix</code></h3>
+
+## Installation
+
+### Step 1: Run the following command to create a new site
+
+```shell
+gatsby new my-site arshad/gatsby-starter-phoenix
+```
+
+### Step 2: Configure `.env`
+
+Copy `.env.example` to `.env` and update `SITE_URL`.
+
+### Step 3: Start developing
+
+```shell
+cd my-site/
+gatsby develop
+```
+
+## Customization
+
+Check out the docs for [@arshad/gatsby-theme-phoenix](https://github.com/arshad/gatsby-themes/tree/master/themes/gatsby-theme-phoenix).
+
+## Support
+
+Create an issue on the main repo [@arshad/gatsby-themes](https://github.com/arshad/gatsby-themes/issues).
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,67 +1,82 @@
-require("dotenv").config()
-
-const config = {
-    title: `Phoenix.`,
-    description: `A free personal blogging theme for Gatsby with great typography and dark mode.`,
-    siteUrl: process.env.SITE_URL,
-    startUrl: "/",
-    postsPerPage: 5,
-    copyright: `© YYYY Phoenix. All rights reserved.`,
-    icon: `assets/images/icon.png`,
-    colors: {
-        primary: "#3C64F1",
-    },
-    fonts: {
-        text: ["Open Sans", "Arial", "sans-serif"],
-        heading: ["Alegreya Sans", "sans-serif"],
-        branding: ["Bowlby One SC", "cursive"],
-    },
-    menuLinks: [
-        {
-            name: "Home",
-            link: "/",
-        },
-        {
-            name: "Projects",
-            link: "/projects",
-        },
-        {
-            name: "About",
-            link: "/about",
-        },
-        {
-            name: "Contact",
-            link: "/contact",
-        },
-    ],
-    socialLinks: [
-        {
-            name: "Twitter",
-            url: "https://twitter.com/arshadcn",
-            icon: "twitter",
-        },
-        {
-            name: "Github",
-            url: "https://github.com/arshad",
-            icon: "github",
-        },
-        {
-            name: "Youtube",
-            url: "https://youtube.com",
-            icon: "youtube",
-        },
-    ],
-}
+require(`dotenv`).config()
 
 module.exports = {
-    siteMetadata: config,
-    plugins: [
-        {
-            resolve: "@arshad/gatsby-theme-phoenix",
-            options: {
-                ...config,
-                contentPath: process.env.MDX_CONTENT_PATH,
-            },
-        },
+  siteMetadata: {
+    title: `Phoenix.`,
+    description: `A personal blogging theme for Gatsby with great typography and dark mode.`,
+    siteUrl: process.env.SITE_URL,
+    startUrl: `/`,
+    copyright: `© YYYY Phoenix. All rights reserved.`,
+    icon: `src/images/icon.png`,
+    color: `#3C64F1`,
+    menuLinks: [
+      {
+        name: `Home`,
+        link: `/`,
+      },
+      {
+        name: `Blog`,
+        link: `/blog`,
+      },
+      {
+        name: `Podcast`,
+        link: `/podcast`,
+      },
+      {
+        name: `Porfolio`,
+        link: `/portfolio`,
+      },
+      {
+        name: `Photography`,
+        link: `/photography`,
+      },
     ],
+    socialLinks: [
+      {
+        name: `Twitter`,
+        url: `https://twitter.com`,
+        icon: `twitter`,
+      },
+      {
+        name: `Github`,
+        url: `https://github.com`,
+        icon: `github`,
+      },
+      {
+        name: `Youtube`,
+        url: `https://youtube.com`,
+        icon: `youtube`,
+      },
+    ],
+  },
+  plugins: [
+    // This is a list of all themes that this starter is using.
+    // To disable a theme, remove it here and run `yarn remove @arshad/gatsby-theme-NAME`.
+    `@arshad/gatsby-theme-blog-core`,
+    `@arshad/gatsby-theme-page-core`,
+    `@arshad/gatsby-theme-portfolio-core`,
+    `@arshad/gatsby-theme-photo-core`,
+    {
+      resolve: `@arshad/gatsby-theme-podcast-core`,
+      options: {
+        feedUrl: `https://feeds.megaphone.fm/travelgenius`,
+        podcast: {
+          name: `Travel Genuis`,
+          description: `Eligendi nisi nobis nisi voluptate. Corporis deserunt provident hic numquam. Veritatis vero necessitatibus adipisci cumque voluptate rerum at.`,
+          image: `assets/images/podcast.jpg`,
+          social: [
+            {
+              name: `Apple Podcast`,
+              url: `https://itunes.apple.com`,
+            },
+            {
+              name: `Google Podcast`,
+              url: `https://podcasts.google.com`,
+            },
+          ],
+        },
+      },
+    },
+    `@arshad/gatsby-theme-phoenix`,
+  ],
 }
